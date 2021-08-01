54ee1071c70b2d707b22c2bf955162a8cbe7a15c
faskan <faisalkhanthayub@gmail.com>
2021-08-01 11:45:17 +0200

    first commit

---
+++ src/main/java/com/example/featureflag/FeatureFlagApplication.java
@@ -0,0 +1,55 @@
+package com.example.featureflag;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class FeatureFlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagApplication.class, args);
+	}
+
+}
+
+@Configuration
+class MyConfiguration {
+
+	@Bean
+	@ConditionalOnProperty(prefix = "features", name = "service", havingValue = "true")
+	public MessageService messageService(){
+		return new MessageService();
+	}
+}
+
+@RestController
+class MyRestController {
+	private MessageService messageService;
+
+	public MyRestController(Optional<MessageService> optionalMessageService) {
+		optionalMessageService.ifPresent(service -> this.messageService = service);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return messageService.getMessage();
+	}
+}
+
+class MessageService {
+
+	public String getMessage() {
+		return "Hello World";
+	}
+}
+
+
